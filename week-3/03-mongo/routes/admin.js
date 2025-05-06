0f0ea38e3951cd52b0a657872ce70e5247b8a174
Javed Akhtar <conn.javed@gmail.com>
2025-05-07 01:22:21 +0530

    week-3/03-mongo completed

--- week-3/03-mongo/routes/admin.js
+++ week-3/03-mongo/routes/admin.js
@@ -1,18 +1,53 @@
-const { Router } = require("express");
+const { Router, response } = require("express");
 const adminMiddleware = require("../middleware/admin");
+const { Admin, Course } = require("../db");
 const router = Router();
 
 // Admin Routes
 router.post('/signup', (req, res) => {
     // Implement admin signup logic
+    const username = req.body.username;
+    const password = req.body.password;
+
+    Admin.create({
+        username,
+        password
+    })
+    .then(() => {
+        res.json({
+            message: 'Admin created successfully'
+        })
+    })
 });
 
 router.post('/courses', adminMiddleware, (req, res) => {
     // Implement course creation logic
+    const title = req.body.title;
+    const description = req.body.description;
+    const price = req.body.price;
+    const imgLink = req.body.imgLink;
+
+    Course.create({
+        title,
+        description,
+        price,
+        imgLink
+    })
+    .then((response) => {
+        res.json({
+            message: 'Course created successfully', courseId: response._id
+        })
+    })
 });
 
 router.get('/courses', adminMiddleware, (req, res) => {
     // Implement fetching all courses logic
+    Course.find()
+    .then((response) => {
+        res.json({
+            courses: response
+        })
+    })
 });
 
 module.exports = router;
